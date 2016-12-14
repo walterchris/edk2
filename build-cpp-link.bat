33814464c5ea29db57d931d5d8e69afdee3d0ac3
Matt DeVillier <matt.devillier@gmail.com>
2016-12-13 19:42:02 -0600

    Add batch files/scripts for building
    Signed-off-by: Matt DeVillier <matt.devillier@gmail.com>

---
+++ build-cpp-link.bat
@@ -0,0 +1,6 @@
+set "var=%cd%"
+rmdir /s /q .\Build
+copy ..\blobs\mainboard\google\link\vbt.bin CorebootPayloadPkg\vbt.bin
+copy ..\blobs\soc\intel\ivb\IntelGopDriver.efi CorebootPayloadPkg\IntelGopDriver.efi
+build -a IA32 -a X64 -p CorebootPayloadPkg\CorebootPayloadPkgIa32X64.dsc -b RELEASE -t VS2015x86 -n 4
+copy .\Build\CorebootPayloadPkgX64\RELEASE_VS2015x86\FV\UEFIPAYLOAD.fd z:\firmware\tianocore\UEFIPAYLOAD-link.fd
