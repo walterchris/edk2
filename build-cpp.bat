dca1f59aeefae40f82c1f0656ba6a4a888e1c53f
Matt Devo <matt.devillier@gmail.com>
2016-12-13 19:42:02 -0600

    Add batch files for building

---
+++ build-cpp.bat
@@ -0,0 +1,7 @@
+call build-cpp-byt.bat
+call build-cpp-chell.bat
+call build-cpp-hswbdw-book.bat
+call build-cpp-hswbdw-box.bat
+call build-cpp-ivb-book.bat
+call build-cpp-snb-book.bat
+call build-cpp-snb-box.bat
